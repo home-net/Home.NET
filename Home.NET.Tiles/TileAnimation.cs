9d101d14853d890ef176dc639a0591adb71c9d62
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-02-02 13:13:03 +0200

    Add TileAnimations extension

---
+++ Home.NET.Tiles/TileAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Home.NET.Tiles
+{
+    public static class TileAnimation
+    {
+        public static void LiveTileAnimation(this Tile tile, bool Enable = false)
+        {
+
+        }
+    }
+}
